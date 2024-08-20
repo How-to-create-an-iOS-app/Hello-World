19d9bf223016ef9a9dadb4efa76a1ecb42966f3d
How-to-create-an-iOS-app <support@howtocreateaniosapp.com>
2024-08-20 09:56:02 -0300

    Final project is done

--- projects/final/HelloWorld/HelloWorld/ViewController.swift
+++ projects/final/HelloWorld/HelloWorld/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let showHelloWorld = "Show Hello World"
+    let hideHelloWorld = "Hide Hello World"
+    
+    var areImageAndLabelHidden = true {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var button: UIButton!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: - Methods
+    
+    func updateUI() {
 
+        // image
+        stackView.isHidden = areImageAndLabelHidden
+
+        // button
+        let buttonTitle = areImageAndLabelHidden
+                        ? showHelloWorld
+                        : hideHelloWorld
+        
+        button.setTitle(buttonTitle, for: .normal)
+   
+    }
+
+    // MARK: - Actions
+    
+    @IBAction func showImageAndLabel(_ sender: UIButton) {
+        areImageAndLabelHidden.toggle()
+    }
+    
 }
-
